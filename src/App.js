b3fea3bb0102f242f767ec2858d216a339e652e8
anikobakhidze <kobakhidzeana23@gmail.com>
2024-02-06 18:00:15 +0400

    change home page style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import AppRoutes from "./AppRoutes";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-
+import { PacmanLoader } from "react-spinners";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import "./App.css";
 function App() {
+  const [loading, setLoading] = useState(false);
+  const location = useLocation;
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => setLoading(false), 1000);
+  }, [location.pathname, setLoading]);
   return (
     <>
-      <Header />
-      <AppRoutes />
-      <Footer />
+      {loading ? (
+        <div className="loaderContainer">
+          <PacmanLoader color="#FFC24B" size={40} />
+        </div>
+      ) : (
+        <>
+          <Header />
+          <AppRoutes />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
